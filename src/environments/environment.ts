e0c56d2651c2df78d27224f6a89890e7d229c2ae
Stefan Popoiu <stefan.popoiu@gmail.com>
2023-03-21 15:02:29 +0200

    setup firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'notesproject-f5eb0',
+    appId: '1:742083984134:web:3ba62946eefe2dc9a8bc87',
+    storageBucket: 'notesproject-f5eb0.appspot.com',
+    apiKey: 'AIzaSyCxPp-cjdqBITGui9QvjYbUns2mMTQofm0',
+    authDomain: 'notesproject-f5eb0.firebaseapp.com',
+    messagingSenderId: '742083984134',
+  },};
